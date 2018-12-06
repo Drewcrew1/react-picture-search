83575247aad36010bcf804912794b27a78024a3d
Drew <drewharper29@gmail.com>
2018-12-06 11:46:11 -0500

    added axios library added query config

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
-const App = () => {
-    return <div className="ui container"><SearchBar/></div>;
-};
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization : 'Client-ID 0164f4337dfd60ea94588c0ac8cfc50f64b89f426173c57a5a24b043d8516bcb'
+
+            }
+        });
+    }
+
+    render()
+    {
+        return (
+            <div className="ui container">
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
+}
 
 export default App;
